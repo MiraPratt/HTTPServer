2b4bad6a6ad9279e484795cb79095e862d223cb7
MiraPratt <joseph.pratt@case.edu>
2018-12-23 11:30:16 -0500

    Display ASCII Art

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -1,17 +1,37 @@
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HTTPServer {
 
 	public static void main(String[] args) throws IOException {
 
-		ServerSocket ser = new ServerSocket(8080);
-		System.out.println("Listening on port 80...");
+		ServerSocket ser = new ServerSocket(1025);
+		System.out.println("Listening on port 1025...");
 		
 		while(true) {
 			try(Socket cli = ser.accept()){
-			String res = "HTTP/1.1 200 OK\r\n\r\n You just loaded a webpage from Mira's computer.";
+			String res = "HTTP/1.1 200 OK\r\n\r\n    _______________________-------------------                       `\\\n" + 
+					" /:--__                                                              |\n" + 
+					"||< > |                                   ___________________________/\n" + 
+					"| \\__/_________________-------------------                         |\n" + 
+					"|                                                                  |\n" + 
+					" |                       THE LORD OF THE RINGS                      |\n" + 
+					" |                                                                  |\n" + 
+					" |      \"Three Rings for the Elven-kings under the sky,             |\n" + 
+					"  |        Seven for the Dwarf-lords in their halls of stone,        |\n" + 
+					"  |      Nine for Mortal Men doomed to die,                          |\n" + 
+					"  |        One for the Dark Lord on his dark throne                  |\n" + 
+					"  |      In the Land of Mordor where the Shadows lie.                 |\n" + 
+					"   |       One Ring to rule them all, One Ring to find them,          |\n" + 
+					"   |       One Ring to bring them all and in the darkness bind them   |\n" + 
+					"   |     In the Land of Mordor where the Shadows lie.                |\n" + 
+					"  |                                              ____________________|_\n" + 
+					"  |  ___________________-------------------------                      `\\\n" + 
+					"  |/`--_                                                                 |\n" + 
+					"  ||[ ]||                                            ___________________/\n" + 
+					"   \\===/___________________--------------------------\"";
 			cli.getOutputStream().write(res.getBytes("UTF-8"));
 			}
 		}
